1ec13ca5914fe2154f0b3d412a0965a35c328985
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2019-07-20 15:47:39 +0300

    Tehtävä 1.1 & 1.2 done

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Hello = props => {
+  let age = 0;
+  if (props.age == null) {
+    age = 'unknown';
+  } else {
+    age = props.age;
+  }
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Matti!" age="25" />
+      <Hello name="Maija!" age={12 + 10} />
+      <Hello name="You!" />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
