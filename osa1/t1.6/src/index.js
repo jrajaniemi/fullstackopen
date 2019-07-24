870a7f3ce35135d0949ce99df29fb96dd3d0a550
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2019-07-24 11:35:18 +0300

    t1.9 step4

--- osa1/t1.6/src/index.js
+++ osa1/t1.6/src/index.js
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Statistics = props => {
   const { value, text } = props;
-
+  console.log(value, text);
   return (
-    <div class="row">
-      <div class="col-md-4">{text}</div>
-      <div class="col-md-4">{value}</div>
+    <div className="row">
+      <div className="col-md-4">{text}</div>
+      <div className="col-md-4">{value}</div>
     </div>
   );
 };
 
 const Button = props => (
-  <button class="btn btn-primary m-1 col-3" onClick={props.handleClick}>
+  <button className="btn btn-primary m-1 col-3" onClick={props.handleClick}>
     {props.text}
   </button>
 );
@@ -36,29 +36,46 @@ const App = props => {
     setBad(bad + 1);
   };
 
-  return (
-    <div class="container mt-3">
-      <div class="row">
-        <div class="col-12">
-          <Button handleClick={handleGood} text="Good" />
-          <Button handleClick={handleNeutral} text="Netral" />
-          <Button handleClick={handleBad} text="Bad" />
+  if (good > 0 || neutral > 0 || bad > 0) {
+    return (
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-12">
+            <Button handleClick={handleGood} text="Good" />
+            <Button handleClick={handleNeutral} text="Netral" />
+            <Button handleClick={handleBad} text="Bad" />
+          </div>
+        </div>
+        <Statistics text="Good" value={good} />
+        <Statistics text="Neutral" value={neutral} />
+        <Statistics text="Bad" value={bad} />
+        <Statistics text="All" value={good + neutral + bad} />
+        <Statistics
+          text="Average"
+          value={(good + bad * -1) / (good + neutral + bad)}
+        />
+        <Statistics
+          text="Positive"
+          value={(good * 100) / (good + neutral + bad)}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-12">
+            <Button handleClick={handleGood} text="Good" />
+            <Button handleClick={handleNeutral} text="Netral" />
+            <Button handleClick={handleBad} text="Bad" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">No feedback given</div>
         </div>
       </div>
-      <Statistics text="Good" value={good} />
-      <Statistics text="Neutral" value={neutral} />
-      <Statistics text="Bad" value={bad} />
-      <Statistics text="All" value={good + neutral + bad} />
-      <Statistics
-        text="Average"
-        value={(good + bad * -1) / (good + neutral + bad)}
-      />
-      <Statistics
-        text="Positive"
-        value={(good * 100) / (good + neutral + bad)}
-      />
-    </div>
-  );
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
