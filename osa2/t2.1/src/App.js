02150238a618cd3a57aeee50d2f81633d9153b66
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2019-07-25 20:36:56 +0300

    t2.4 & 2.5 done

--- osa2/t2.1/src/App.js
+++ osa2/t2.1/src/App.js
@@ -3,39 +3,56 @@ import Course from './components/Course';
 import './App.css';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  };
+  const course = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
+  const courses = course.map((item, i) => {
+    return <Course course={item} />;
+  });
 
   console.log(course);
 
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  );
+  return <div>{courses}</div>;
 };
 
 export default App;
